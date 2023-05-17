72559db73889a2dc9fc560bbbb05971ecb9ba717
ErikaYuri <erikayuri@uol.com.br>
2023-05-16 22:36:45 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+if(document.getElementById("nome").value estiver preenchido && document.getElementById("email").value estiver preenchido){
+    envie os dados
+}else{
+    peça a usuaria que preencha os campo nome e email
+}
