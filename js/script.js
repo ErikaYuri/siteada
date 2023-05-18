22fcd00293f465ff356d06b05218790ff149b0ba
ErikaYuri <erikayuri@uol.com.br>
2023-05-17 22:00:05 -0300

    formulario

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-if(document.getElementById("nome").value != "" && document.getElementById("email").value != ""){
-    alert("Prontinho! Você receberá as novidades por email.")
-}else{
-    alert("Por favor, preencha os campos nome e email!")
+document
+  .getElementById("botaoEnviar")
+  .addEventListener("click", validaFormulario);
+
+function validaFormulario() {
+  if (
+    document.getElementById("nome").value != "" &&
+    document.getElementById("email").value != "" && document.getElementById("telefone").value != ""
+  ) {
+    alert("Prontinho! Você receberá as novidades por email.");
+  } else {
+    alert("Por favor, preencha os campos nome e email!");
+  }
 }
